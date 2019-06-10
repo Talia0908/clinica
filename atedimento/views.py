02d448c88ca922d78f7dd246f1d9bbd06fa6b5f4
Your Name <you@example.com>
2019-06-10 06:10:38 -0300

    Criação de medicamentos

--- atedimento/views.py
+++ atedimento/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
+from . models import Paciente, Medico, Medicamento
 
 def home(request):
-    return render(request,'home.html') 
+    return render (request, 'home.html')
 
 def paciente_list(request):
-    return render(request,'paciente/list.html') 
+    pacientes = Paciente.objects.all()
+    return render (request, 'paciente/list.html', {'pacientes': pacientes})
 
 def paciente_show(request, id):
-    return render(request, 'paciente/show.html', {'id': id}) 
-
+    return render (request, 'paciente/show.html', {'id': id})
 
 def medico_list(request):
-    return render(request,'medico/list.html')
+    medicos = Medico.objects.all()
+    return render (request, 'medico/list.html', {'medicos': medicos})
 
 def medico_show(request, id):
-    return render(request, 'medico/show.html', {'id': id})
+    return render (request, 'medico/show.html', {'id': id})
 
 def agendamento_list(request):
-    return render(request, 'agendamento/list.html')
-    
+    return render (request, 'agendamento/list.html')
+
+def medicamento_list(request):
+    medicamentos = Medicamento.objects.all()
+    return render (request, 'medicamento/list.html', {'medicamentos': medicamentos})
